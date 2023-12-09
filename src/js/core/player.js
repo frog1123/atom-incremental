30407df0dd27005152e0a4e37fa7d4a5fa462ce3
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-12-09 14:42:55 -0600

    +

--- src/js/core/player.js
+++ src/js/core/player.js
@@ -1,3 +1,19 @@
 let player = {
-  atoms: new Decimal("10")
+  atoms: new Decimal("10"),
+  atomForges: {
+    tier1: {
+      level: new Decimal("0"),
+      cost: new Decimal("10"),
+      costIncrease: new Decimal("100"),
+      multiplier: new Decimal("1"),
+      amountBought: new Decimal("0")
+    },
+    tier2: {
+      level: new Decimal("0"),
+      cost: new Decimal("100"),
+      costIncrease: new Decimal("100"),
+      multiplier: new Decimal("1"),
+      amountBought: new Decimal("0")
+    }
+  }
 };
